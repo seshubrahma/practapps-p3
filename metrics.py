db4992d4c6e6ff119016483c8bf0ccd8da6b6499
brahma.seshu <brahma.seshu@gmail.com>
2019-04-26 17:33:03 -0500

    metrics works! (i think)

--- metrics.py
+++ metrics.py
@@ -5,41 +5,24 @@ import re
 import argparse
 import json
 import sys
-
-
-def normalize_answer(s):
-    """Lower text and remove punctuation, articles and extra whitespace."""
-    def remove_articles(text):
-        return re.sub(r'\b(a|an|the)\b', ' ', text)
-
-    def white_space_fix(text):
-        return ' '.join(text.split())
-
-    def remove_punc(text):
-        exclude = set(string.punctuation)
-        return ''.join(ch for ch in text if ch not in exclude)
-
-    def lower(text):
-        return text.lower()
-
-    return white_space_fix(remove_articles(remove_punc(lower(s))))
+import Levenshtein
 
 
 def f1_score(prediction, ground_truth):
-    prediction_tokens = normalize_answer(prediction).split()
-    ground_truth_tokens = normalize_answer(ground_truth).split()
+    prediction_tokens = prediction.split()
+    ground_truth_tokens = ground_truth.split()
     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
     num_same = sum(common.values())
     if num_same == 0:
-        return 0
-    precision = 1.0 * num_same / len(prediction_tokens)
-    recall = 1.0 * num_same / len(ground_truth_tokens)
+        num_same = 1.0
+    precision = 1.0 * num_same / (len(prediction_tokens) + 1)
+    recall = 1.0 * num_same / (len(ground_truth_tokens) + 1)
     f1 = (2 * precision * recall) / (precision + recall)
     return f1
 
 
 def exact_match_score(prediction, ground_truth):
-    return (normalize_answer(prediction) == normalize_answer(ground_truth))
+    return (1 / (Levenshtein.distance(prediction, ground_truth) + 1))
 
 
 def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
@@ -52,52 +35,48 @@ def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
 
 def evaluate(dataset, predictions):
     f1 = exact_match = total = 0
+
     for article in dataset:
-        print("article", article)
         for paragraph in article['paragraphs']:
-            print("paragraph", paragraph)
             for qa in paragraph['qas']:
-                print("qa", qa)
                 total += 1
-                if qa['id'] not in predictions:
-                    print("qa[id] not in predictions")
-                    message = 'Unanswered question ' + qa['id'] + \
-                              ' will receive score 0.'
-                    print(message, file=sys.stderr)
-                    continue
-                ground_truths = list(map(lambda x: x['text'], qa['answers']))
-                print("groundtruths", ground_truths)
-                prediction = predictions[qa['id']]
-                print("prediction", prediction)
-                exact_match += metric_max_over_ground_truths(
-                    exact_match_score, prediction, ground_truths)
-                print("exactmatch", exact_match)
-                f1 += metric_max_over_ground_truths(
-                    f1_score, prediction, ground_truths)
-                print("f1", f1)
+                # if qa['id'] not in predictions:
+                #     print('Unanswered question ' + qa['id'] + ' will receive score 0.')
+                #     continue
+                ground_truths = list(map(lambda x: x['text'], qa['answers'])) #list that has all the ground truth answers 
+
+                #if anything in predictions matches anything in the ground_truths list
+                most_similar_ratio = 0
+                for uuid in predictions:
+                    for answer in range(len(predictions[uuid])):
+                        current_ratio = Levenshtein.ratio(ground_truths[0], predictions[uuid][answer]['text'])
+                        if current_ratio > most_similar_ratio:
+                            most_similar_ratio = current_ratio
+                            most_similar_prediction = predictions[uuid][answer]['text']
+
+                # qa_id = qa['id'].encode('ascii','ignore')
+                prediction = most_similar_prediction
+                exact_match += metric_max_over_ground_truths(exact_match_score, prediction, ground_truths)
+                f1 += metric_max_over_ground_truths(f1_score, prediction, ground_truths)
 
     exact_match = 100.0 * exact_match / total
-    print("exact match final", exact_match)
     f1 = 100.0 * f1 / total
 
     return {'exact_match': exact_match, 'f1': f1}
 
 
-#originally for 1.1 version
 if __name__ == '__main__':
     expected_version = '2.0'
-    parser = argparse.ArgumentParser(
-        description='Evaluation for SQuAD ' + expected_version)
+    parser = argparse.ArgumentParser(description='Evaluation for SQuAD ' + expected_version)
     parser.add_argument('dataset_file', help='Dataset file')
     parser.add_argument('prediction_file', help='Prediction File')
     args = parser.parse_args()
+    
     with open(args.dataset_file) as dataset_file:
         dataset_json = json.load(dataset_file)
-        if (dataset_json['version'] != expected_version):
-            print('Evaluation expects v-' + expected_version +
-                  ', but got dataset with v-' + dataset_json['version'],
-                  file=sys.stderr)
         dataset = dataset_json['data']
+    
     with open(args.prediction_file) as prediction_file:
         predictions = json.load(prediction_file)
+    
     print(json.dumps(evaluate(dataset, predictions)))
